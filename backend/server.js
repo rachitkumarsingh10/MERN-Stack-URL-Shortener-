3c5e35b5fe8b705916a53eed039989a9695351d5
Rachit Kumar Singh <rachitkumarsingh10@gmail.com>
2026-08-27 13:51:56 +0530

    Configuring  Mongodb Atlas

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const dns = require("dns");
+
+// Use Google DNS for this Node.js application
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const urlRoutes = require("./routes/urlRoutes");
 
@@ -13,7 +17,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// API routes
 app.use("/api/urls", urlRoutes);
+
+// Redirect routes
 app.use("/r", urlRoutes);
 
 app.get("/", (req, res) => {
@@ -24,6 +31,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅ MongoDB connected");
+
     app.listen(PORT, () => {
       console.log(`🚀 Server running on port ${PORT}`);
     });
