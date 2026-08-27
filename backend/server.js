35e7623a829b55fb535eacf2d96551fab13cd98a
Rachit Kumar Singh <rachitkumarsingh10@gmail.com>
2026-08-27 14:03:54 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,28 +5,37 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const dns = require("dns");
 
-// Use Google DNS for this Node.js application
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
-
 const urlRoutes = require("./routes/urlRoutes");
+
+// Use Google DNS for MongoDB SRV lookup
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+// CORS configuration
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "DELETE"],
+  })
+);
+
 app.use(express.json());
 
 // API routes
 app.use("/api/urls", urlRoutes);
 
-// Redirect routes
+// Short URL redirect routes
 app.use("/r", urlRoutes);
 
+// Test route
 app.get("/", (req, res) => {
   res.json({ message: "URL Shortener API is running 🚀" });
 });
 
+// Connect MongoDB and start server
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
